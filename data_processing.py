2e617d70b39252ee0bc296dfa3c648ff7f5970e5
satchand-sl <shansat@yahoo.com>
2022-03-22 13:55:36 +0530

    adding sum function to data processing.

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,10 @@
 
 print ("Add all the data processing code related to data processing!")
+
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return c
+
+print ("added new function sum which is not correct")
