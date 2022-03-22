eec9ce60afe98ca1e67b19cefb04c92b56f45be5
satchand-sl <shansat@yahoo.com>
2022-03-22 15:42:37 +0530

    Revert "adding sum function to data processing."
    This reverts commit 2e617d70b39252ee0bc296dfa3c648ff7f5970e5.

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,2 @@
 
 print ("Add all the data processing code related to data processing!")
-
-a=5
-b=7
-def sum(a,b):
-    c=a+b
-    return c
-
-print ("added new function sum which is not correct")
